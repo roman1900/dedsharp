b6ebfad8cdca392e636ff79301a212c19707dee8
Matt Marschall <mmarschall@redmars.com.au>
2021-07-26 12:03:12 +1000

    Implemented render_editor_into_fgb

--- Program.cs
+++ Program.cs
@@ -14,12 +14,10 @@ namespace dedsharp
         static Editor editor = new Editor();
         static Window window;
 
-        static Vector2 camera_pos = new Vector2();
-        static Vector2 camera_vel = new Vector2();
-        const int SCREEN_WIDTH = 800;
+        public const int SCREEN_WIDTH = 800;
         const int SCREEN_HEIGHT = 600;
         const int FPS = 60;
-        const float DELTA_TIME = 1.0f / FPS;
+        public const float DELTA_TIME = 1.0f / FPS;
         static void Main(string[] args)
         {
             
@@ -39,19 +37,15 @@ namespace dedsharp
             nativeWindowSettings.Title="Text Editor";
             nativeWindowSettings.Size = new Vector2i(SCREEN_WIDTH,SCREEN_HEIGHT);
 
-            using (window = new Window(GameWindowSettings.Default,NativeWindowSettings.Default))
+            using (window = new Window(GameWindowSettings.Default,NativeWindowSettings.Default,editor))
             {
-                window.UpdateFrequency=60;
+                window.UpdateFrequency=FPS;
                 window.KeyDown +=  keyPress;
                 window.TextInput += textInput;
                 
                 window.Run();
                 
-                Vector2 cursor_pos = new Vector2(editor.cursor_col * Font.FONT_CHAR_WIDTH * Font.FONT_SCALE, 
-                                                editor.cursor_row * Font.FONT_CHAR_HEIGHT * Font.FONT_SCALE);
-                camera_vel = Vector2.Multiply(Vector2.Subtract(cursor_pos,camera_pos),2.0f);
-                camera_pos = Vector2.Add(camera_pos,Vector2.Multiply(camera_vel,DELTA_TIME));
-
+                
             }
         }
         static void keyPress(KeyboardKeyEventArgs k)
--- Window.cs
+++ Window.cs
@@ -1,5 +1,6 @@
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.Common;
+using OpenTK.Mathematics;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Input;
 using OpenTK.Graphics;
@@ -16,23 +17,82 @@ namespace dedsharp
     {
 		public static uint FREE_GLYPH_FONT_SIZE = 64;
 		public static int ZOOM_OUT_GLYPH_THRESHOLD = 30;
-
+		public static Vector2 camera_pos = new Vector2();
+        public static Vector2 camera_vel = new Vector2();
+        public static float camera_scale_vel = 0.0f;
+		public static float camera_scale = 3.0f;
+		Editor editor;
+		private FreeGlyphBuffer fgb;
 		public static void MessageCallback(DebugSource source, DebugType type, int id, DebugSeverity severity, int length, System.IntPtr message, System.IntPtr userParam )
 		{
 			Console.WriteLine($"GL CALLBACK: type = {type} severity = {severity} message = {Marshal.PtrToStringAuto(message)}");
 		}
 		
-		public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings,nativeWindowSettings) { }
+		public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings,Editor editor) : base(gameWindowSettings,nativeWindowSettings) {
+			this.editor = editor;
+		}
+		private void render_editor_into_fgb()
+		{
+			int w,h;
+			w = this.Size.X;
+			h = this.Size.Y;
+			float max_line_len = 0.0f;
+			fgb.free_glyph_buffer_use();
+			for (int row= 0; row < editor.size; ++row) 
+			{
+				Editor.Line line = editor.lines[row];
+				Vector2 begin = new Vector2(0, -(float)row * FREE_GLYPH_FONT_SIZE);
+				Vector2 end = begin;
+				fgb.free_glyph_buffer_render_line_sized(line.chars,end,new Vector4(1.0f),new Vector4(0.0f));
+				float line_len = Math.Abs(end.X - begin.X);
+				if (line_len > max_line_len){
+					max_line_len = line_len;
+				}
+			}
+			fgb.free_glyph_buffer_sync();
+			fgb.free_glyph_buffer_draw();
+
+			Vector2 cursor_pos = new Vector2(0.0f);
+			cursor_pos.Y = - editor.cursor_row * FREE_GLYPH_FONT_SIZE;
+			if (editor.cursor_row < editor.size){
+				Editor.Line line = editor.lines[editor.cursor_row];
+				cursor_pos.X = fgb.free_glyph_buffer_cursor_pos(line.chars,new Vector2(0.0f,cursor_pos.Y),editor.cursor_col);
+			}
+			//TODO: cursor implementation
+
+			float target_scale = 3.0f;
+			if (max_line_len > 0.0f) {
+				target_scale = Program.SCREEN_WIDTH / max_line_len;
+			}
+			if (target_scale > 3.0f){
+				target_scale = 3.0f;
+			}
+			camera_vel = Vector2.Multiply(Vector2.Subtract(cursor_pos, camera_pos), 2.0f);
+			camera_scale_vel = (target_scale - camera_scale) * 2.0f;
+			camera_pos = Vector2.Add(camera_pos, Vector2.Multiply(camera_vel,Program.DELTA_TIME));
+			camera_scale = camera_scale + camera_scale_vel * Program.DELTA_TIME;
+		}
 		protected override void OnUpdateFrame(FrameEventArgs e)
 		{
 			
 			base.OnUpdateFrame(e);
+			GL.ClearColor(0.0f,0.0f,0.0f,1.0f);
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+			
+			SwapBuffers();
 		}
 
+		protected override void OnResize(ResizeEventArgs e)
+		{
+			base.OnResize(e);
+			GL.Viewport(0,0,e.Width,e.Height);
+		}
 		protected override void OnLoad()
 		{
 
 			base.OnLoad();
+			
+
 			//TODO: exception checking
 			Library library = new Library();
 			const string font_file_path = "./VictorMono-Regular.ttf";
@@ -46,8 +106,7 @@ namespace dedsharp
 			GL.Enable(EnableCap.DebugOutput);
 			DebugProc MCB = MessageCallback;
 			GL.DebugMessageCallback(MCB,new System.IntPtr(0));
-			FreeGlyphBuffer fgb = new FreeGlyphBuffer(
-                           face,
+			fgb = new FreeGlyphBuffer(face,
                            "./shaders/free_glyph.vert",
                            "./shaders/free_glyph.frag");
 			
